e296b5e4ad0b7c4be88d89d52d9c9865d9961948
dasariabhiram <dasariabhiramprojects@gmail.com>
2024-02-08 12:33:39 +0530

    meta tag update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,15 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'JS Mastery',
-  description: 'JS Mastery Resources',
+  title: 'Tech resources',
+  description: 'Tech Resources',
   other: {
     'theme-color': '#0d1117',
     "color-scheme": "dark only",
-    "twitter:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    "twitter:image": 'https://ibb.co/3MRrGLp',
     "twitter:card": "summary_large_image",
-    "og:url": "jsmastery.pro",
-    "og:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    "og:url": "Tech Resources",
+    "og:image": 'https://ibb.co/3MRrGLp',
     "og:type": "website",
   }
 }
